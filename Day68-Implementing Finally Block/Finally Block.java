17c85422ebbd53f5207fbd29f56a574edfecf9a2
Saikishore21 <114654354+Saikishore21@users.noreply.github.com>
2022-12-20 08:52:00 -0800

    Create Finally Block.java

---
+++ Day68-Implementing Finally Block/Finally Block.java
@@ -0,0 +1,26 @@
+class FinallyBlock
+{
+    public static void main (String args[]) 
+    {
+        //try block 
+        try
+        {
+            System.out.println ("<<<<=====::Try Block::======>>>>");
+            int data = 125 / 5;
+            System.out.println ("Result:" + data);
+        } 
+        //catch block
+        catch (NullPointerException e) 
+        {
+            System.out.println ("<<<====::Catch Block::=====>>>>");
+            System.out.println (e);
+        } 
+        //finally block
+        finally 
+        {
+            System.out.println ("<<<<======:: Finally Block::=====>>>>");
+            System.out.println ("<<<====No Exception::finally block executed===>>>");
+        } 
+        System.out.println ("!!!!!rest of the code...!!!!");
+    } 
+}
