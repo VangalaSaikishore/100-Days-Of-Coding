a13ac2712b2e0d216a69e91447628f52469f6e95
Saikishore21 <114654354+Saikishore21@users.noreply.github.com>
2022-12-23 20:56:19 -0800

    Create Throws keyword.java

---
+++ Day72-Implementing Throws Keyword/Throws keyword.java
@@ -0,0 +1,29 @@
+import java.io.IOException; 
+class throwsKeyword
+{ 
+void m()throws IOException
+{ 
+throw new IOException("<<<<===device error===>>>>");//checked exception 
+} 
+void n()throws IOException
+{ 
+m(); 
+} 
+void p()
+{ 
+try
+{ 
+n(); 
+}
+catch(Exception e)
+{
+System.out.println("<<<<====exception handled=====>>>>");
+} 
+}
+ public static void main(String args[])
+{ 
+ throwsKeyword t=new throwsKeyword(); 
+t.p(); 
+System.out.println("<<<<====normal flow!!!=====>>>>"); 
+} 
+}
