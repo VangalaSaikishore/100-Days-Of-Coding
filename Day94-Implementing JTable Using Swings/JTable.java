604af0c97528d652fdad7c7705decfa28b7a19ca
Saikishore21 <114654354+Saikishore21@users.noreply.github.com>
2023-01-18 01:05:53 -0800

    Create JTable.java

---
+++ Day94-Implementing JTable Using Swings/JTable.java
@@ -0,0 +1,25 @@
+import javax.swing.*; 
+public class TableExample
+{ 
+JFrame f; 
+TableExample()
+{ 
+ f=new JFrame(); 
+ String data[][]={ {"101","Kishore","980000"}, 
+ {"102","Sahan","780000"}, 
+ {"101","Krishna","700000"},
+ {"102","Sairaj","550000"}
+}; 
+ String column[]={"ID","NAME","SALARY"}; 
+JTable jt=new JTable(data,column); 
+jt.setBounds(30,40,200,300); 
+JScrollPane sp=new JScrollPane(jt); 
+f.add(sp); 
+f.setSize(300,400); 
+f.setVisible(true); 
+} 
+public static void main(String[] args) 
+{ 
+ new TableExample(); 
+} 
+}
